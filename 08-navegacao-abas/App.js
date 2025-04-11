5145bc3dcb17164583a20acd8cfbbaf9b4bce2fe
kesiaoliveira21 <kesia.silva@iesb.edu.br>
2025-04-10 21:06:32 -0300

    comitando aula 8

--- 08-navegacao-abas/App.js
+++ 08-navegacao-abas/App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import React from 'react'
+import { PaperProvider } from 'react-native-paper'
+import { Ionicons } from '@expo/vector-icons'
+
+import HomeScreen from './screens/HomeScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import SettingsScreen from './screens/SettingsScreen'
+
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <NavigationContainer>
+        <Tab.Navigator>
+
+          <Tab.Screen
+            name='HomeScreen'
+            component={HomeScreen}
+            options={{
+              title: 'Tela de Início',
+              headerTitleAlign: 'center',
+              headerTintColor: 'white',
+              headerStyle: {
+                backgroundColor: 'tomato'
+              },
+              tabBarIcon: ({ color, size }) => <Ionicons name='home' color={color} size={size} />
+            }}
+          />
+
+          <Tab.Screen
+            name='ProfileScreen'
+            component={ProfileScreen}
+            options={{
+              title: 'Perfil',
+              tabBarIcon: ({ color, size }) => <Ionicons name='person' color={color} size={size} />
+            }}
+          />
+
+          <Tab.Screen
+            name='SettingsScreen'
+            component={SettingsScreen}
+            options={{
+              title: 'Configurações',
+              tabBarIcon: ({ color, size }) => <Ionicons name='cog' color={color} size={size} />
+            }}
+            />
+
+        </Tab.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
